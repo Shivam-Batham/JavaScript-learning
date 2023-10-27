87331eb99c15cb485adb1c80c3e20ef273a610a9
Shivam Batham <117800149+Shivam-Batham@users.noreply.github.com>
2023-10-27 14:45:57 +0000

    First Commit

---
+++ 4_Const_let_var.js
@@ -0,0 +1,6 @@
+const accountId = 12344;
+let accountName = "Saving";
+var accountBalance = 123;
+accountHolder = "Shivam Batham";
+let Bank;
+console.table([accountId,accountHolder,accountName,accountBalance,Bank]);
